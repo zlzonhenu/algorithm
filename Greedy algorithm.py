a928e333c884e503b327124888b59506ad30a4e9
zlzonhenu <chochb123@gmail.com>
2020-09-02 23:05:50 +0900

    알고리즘 공부 시작

---
+++ Greedy algorithm.py
@@ -0,0 +1,23 @@
+#큰수의 법칙
+#p92
+N, M, K = map(int, input().split())
+number = list(map(int, input().split()))
+
+#제일 큰수, 두번째로 큰수 정하기
+number.sort()
+first = number[-1]
+second = number[-2]
+
+sum = 0
+while True:
+    for i in range(K):#제일 큰수 K번 반복가능
+        if M == 0:#M이 0이면 반복문 탈출
+            break
+        sum += first
+        M -= 1
+    if M == 0:
+        break
+    sum += second#두번째로 큰수 한번만 사용
+    M -= 1
+
+print(sum)
