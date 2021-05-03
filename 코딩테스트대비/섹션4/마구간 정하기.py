129c05f29f2825cf6ceebcb2a87e3cafb0ba81dd
zlzonhenu <chochb123@gmail.com>
2021-05-04 00:05:54 +0900

    마구간 수정

--- 코딩테스트대비/섹션4/마구간 정하기.py
+++ 코딩테스트대비/섹션4/마구간 정하기.py
@@ -9,11 +9,13 @@ rt = locs[-1]
 res = 0
 while lt <= rt:
     cnt = 1
+    ep = locs[0]
     mid = (lt+rt) // 2
     for i in range(1, n):
-        dif = locs[i] - locs[0]
+        dif = locs[i] - ep
         if mid <= dif:
             cnt += 1
+            ep = locs[i]
     if cnt < m:
         rt = mid - 1
     else:
