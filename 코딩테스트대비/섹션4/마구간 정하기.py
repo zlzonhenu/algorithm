6087a618ff99dfdf349a7c14fa135b61f568aa5e
zlzonhenu <chochb123@gmail.com>
2021-05-03 23:59:45 +0900

    마구간 정하기 추가

---
+++ 코딩테스트대비/섹션4/마구간 정하기.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+locs = []
+for _ in range(n):
+    loc = int(input())
+    locs.append(loc)
+locs.sort()
+lt = 1
+rt = locs[-1]
+res = 0
+while lt <= rt:
+    cnt = 1
+    mid = (lt+rt) // 2
+    for i in range(1, n-1):
+        dif = locs[i] - locs[0]
+        if mid <= dif:
+            cnt += 1
+    if cnt < m:
+        rt = mid - 1
+    else:
+        res = max(mid, res)
+        lt = mid + 1
+
+print(res)
